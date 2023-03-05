d3b0e5103a8a883807c1556a72612cbb42219ecb
ModerWarfer <86485677+ModerWarfer@users.noreply.github.com>
2023-03-05 17:34:08 -0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -5,10 +5,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ≡ *DONACION*
 puedes donar si quieres ayudar a mantener el bot activo
 
-▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
 ▢ *Mercado Pago Arg*
-• *Link :* https://mpago.la/1F3r6JH
+• *Link :* link.mercadopago.com.ar/abanuls
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendButton(m.chat, don, 'Haga click en boton si quiere comprar o alquilar al bot', img, [['Buy Premium', `${usedPrefix}buyprem`]],m, rpyp)
